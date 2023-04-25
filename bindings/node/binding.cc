09740f0562e25d7e93d4b6b551237e0787c76ad4
Amaan Qureshi <amaanq12@gmail.com>
2023-04-25 05:25:19 -0400

    chore: update bindings

--- bindings/node/binding.cc
+++ bindings/node/binding.cc
@@ -1,28 +1,30 @@
+#include "nan.h"
 #include "tree_sitter/parser.h"
 #include <node.h>
-#include "nan.h"
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_lua();
+extern "C" TSLanguage *tree_sitter_lua();
 
 namespace {
 
 NAN_METHOD(New) {}
 
 void Init(Local<Object> exports, Local<Object> module) {
-  Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
-  tpl->SetClassName(Nan::New("Language").ToLocalChecked());
-  tpl->InstanceTemplate()->SetInternalFieldCount(1);
+    Local<FunctionTemplate> tpl = Nan::New<FunctionTemplate>(New);
+    tpl->SetClassName(Nan::New("Language").ToLocalChecked());
+    tpl->InstanceTemplate()->SetInternalFieldCount(1);
 
-  Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
-  Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_lua());
+    Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
+    Local<Object>   instance =
+        constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
+    Nan::SetInternalFieldPointer(instance, 0, tree_sitter_lua());
 
-  Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("lua").ToLocalChecked());
-  Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
+    Nan::Set(instance, Nan::New("name").ToLocalChecked(),
+             Nan::New("lua").ToLocalChecked());
+    Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
 NODE_MODULE(tree_sitter_lua_binding, Init)
 
-}  // namespace
+} // namespace
