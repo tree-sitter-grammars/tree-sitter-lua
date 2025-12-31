816840c592ab973500ae9750763c707b447e7fef
Christian Clason <c.clason@uni-graz.at>
2025-12-31 13:44:06 +0100

    release v0.4.1

--- src/parser.c
+++ src/parser.c
@@ -12724,7 +12724,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_lua(void) {
     .metadata = {
       .major_version = 0,
       .minor_version = 4,
-      .patch_version = 0,
+      .patch_version = 1,
     },
   };
   return &language;
