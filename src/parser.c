79518ec577be87019401a23880e151ee3b9f5e6e
Christian Clason <c.clason@uni-graz.at>
2026-02-26 18:01:09 +0100

    release v0.5.0

--- src/parser.c
+++ src/parser.c
@@ -13811,8 +13811,8 @@ TS_PUBLIC const TSLanguage *tree_sitter_lua(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 4,
-      .patch_version = 1,
+      .minor_version = 5,
+      .patch_version = 0,
     },
   };
   return &language;
